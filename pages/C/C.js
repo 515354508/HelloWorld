2d5c790db74f94b0d50dd711c8633714bea3009a
515354508 <515354508@qq.com>
2018-12-27 00:06:03 +0800

    base

--- pages/C/C.js
+++ pages/C/C.js
@@ -23,25 +23,25 @@ Page({
     current:0,
 
     state2:[{
-      srr:require('../../assets/avatar.png'),
+      srr:'../../assets/avatar.png',
       title:'拔刀道',
       right:'待付款',
       date:'2018-09-10\t18:00-19:00',
       stress:'科技园\t龙珠四路金谷创业园F栋'
     },{
-      srr:require('../../assets/avatar.png'),
+      srr:'../../assets/avatar.png',
       title:'白鹿尾舞蹈工作室',
       right:'待开始',
       date:'2018-09-10\t18:00-19:00',
       stress:'科技园\t龙珠四路金谷创业园F栋'
     },{
-      srr:require('../../assets/avatar.png'),
+      srr:'../../assets/avatar.png',
       title:'攀岩',
       right:'使用中',
       date:'2018-09-10\t18:00-19:00',
       stress:'科技园\t龙珠四路金谷创业园F栋'
     },{
-      srr:require('../../assets/avatar.png'),
+      srr:'../../assets/avatar.png',
       title:'美国主题馆',
       right:'已完成',
       date:'2018-09-10\t18:00-19:00',
