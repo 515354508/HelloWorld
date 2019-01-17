483c51b3fa28d77f584a1edf963af9f2ac3472d7
515354508 <515354508@qq.com>
2019-01-17 18:26:09 +0800

    base

---
+++ pages/E/E.js
@@ -0,0 +1,17 @@
+Page({
+    data: {
+        state:[{
+          left:'开卡有礼',
+          srr:'../../assets/right.png',
+          top:'200元',
+          mid:'全部产品适用',
+          bottom:'有效期至2018-09-10'
+        },{
+            left:'店庆赠送',
+            srr:'../../assets/right.png',
+            top:'50元',
+            mid:'轰趴主题使用',
+            bottom:'有效期至2018-09-10'
+        }]
+    }
+})
